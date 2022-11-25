d8cd340a9bf6afb1652766235ffcb41ef74e7461
Kafait Ullah <kafaitbhatti.cs@gmail.com>
2022-11-25 16:23:38 +0500

    day 2

--- lib/authorizations_screens/home/view/home.dart
+++ lib/authorizations_screens/home/view/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:payment_app/authorizations_screens/home/view/profile/view/profile.dart';
 import 'package:payment_app/authorizations_screens/home/view/smart_card.dart';
 import 'package:payment_app/authorizations_screens/home/view/pay_bills/view/pay.dart';
 
